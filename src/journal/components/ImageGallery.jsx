ff5db3a0a879b125a08e600ee25ea033c2791a08
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-07-25 18:11:14 -0400

    modificacion de las recorrido de las img

--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -4,7 +4,7 @@ import ImageListItem from '@mui/material/ImageListItem';
 export const ImageGallery = ({images}) => {
   return (
     <ImageList sx={{ width: '100%', height: 450 }} cols={4} rowHeight={164}>
-      {images.map((image,index) => (
+      {images?.map((image,index) => (
         <ImageListItem key={index}>
           <img
             src={`${image}?w=164&h=164&fit=crop&auto=format`}
